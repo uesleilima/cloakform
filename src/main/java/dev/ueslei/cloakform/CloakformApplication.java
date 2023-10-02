2aa1e0d5885f0aaa9826cfa18aa3f443608df07a
Ueslei Lima <ueslei@backbase.com>
2023-10-02 14:04:07 +0200

    Initial Commit

---
+++ src/main/java/dev/ueslei/cloakform/CloakformApplication.java
@@ -0,0 +1,13 @@
+package dev.ueslei.cloakform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloakformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloakformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ueslei/cloakform/CloakformApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.ueslei.cloakform;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+class CloakformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
